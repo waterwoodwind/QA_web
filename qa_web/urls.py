de1c1b3280245852a3c96b2eb7e4838dfdd4a951
waterwindwood <469798015@qq.com>
2016-03-31 19:45:36 +0800

    setting静态文件设置，引入bootstrap、bootstrap-table

--- qa_web/urls.py
+++ qa_web/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    #mysite
+    url(r'^$', 'main.views.home', name='home'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
