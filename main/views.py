ed984eefadcbdee05af8740846f38c3a088a571e
waterwoodwind <469798015@qq.com>
2016-04-06 10:23:45 +0800

    [^]前台表头更换为中文，删去检查者

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django.shortcuts import render
 from main.models import qa_info
 from django.core import serializers
@@ -7,16 +8,29 @@ def home(request):
     query_data = qa_info.objects.all()
     json_data = serializers.serialize("json", query_data,use_natural_foreign_keys=True)
     list_data = json.loads(json_data)
+
+    dict_name_verbose_name = {}
+    for field in qa_info._meta.fields:
+        dict_name_verbose_name[field.name] = field.verbose_name
+        #print field.name, field.verbose_name
+
     upload_data = []
     for item in list_data:
         upload_data.append(item['fields'])
-        print upload_data
+        #print upload_data
 
-    dict_id_verbose_name = {}
-    for field in qa_info._meta.fields:
-        dict_id_verbose_name[field.name] = field.verbose_name
-        print field.name, field.verbose_name
+    chinese_updata = []
+    for item in upload_data:
+        print item
+        dict_updata = {}
+        for key,value in item.items():
+            if dict_name_verbose_name[key] != u"检查者":
+                dict_updata[dict_name_verbose_name[key]] = value
+
+            #print chinese_updata
+        chinese_updata.append(dict_updata)
 
 
-    upload_data = json.dumps(upload_data)
+
+    upload_data = json.dumps(chinese_updata)
     return render(request, 'home.html',{'json_data': upload_data})
