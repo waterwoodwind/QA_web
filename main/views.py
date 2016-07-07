885ffbac6a5c3bec98fc64f74f6c49f8cc7a76ed
waterwoodwind <469798015@qq.com>
2016-07-07 09:13:16 +0800

    [+]抽象views中重复代码为函数

--- main/views.py
+++ main/views.py
@@ -75,7 +75,7 @@ def home(request):
     upload_data = json.dumps(chinese_updata)
     return render(request, 'home.html',{'json_data': upload_data})
 
-def background(request):
+def df_chinese_data():
     exclude_list = []
 
     query_data = qa_info.objects.all().order_by('-data')
@@ -136,146 +136,28 @@ def background(request):
         for key, value in item.items():
             dict_updata[dict_name_verbose_name[key]] = value
 
-                # print chinese_updata
+            # print chinese_updata
         chinese_updata.append(dict_updata)
 
-    upload_data = json.dumps(chinese_updata)
+    return chinese_updata
+
+def background(request):
+
+
+    upload_data = json.dumps(df_chinese_data())
     return render(request, 'background.html', {'json_data': upload_data})
 
 def source(request):
-    exclude_list = [u"检查者", u"责任人", u"ID"]
-
-    query_data = qa_info.objects.all().order_by('-data')
-    json_data = serializers.serialize("json", query_data, use_natural_foreign_keys=True)
-    list_data = json.loads(json_data)
-
-    dict_name_verbose_name = {}
-    columns_set = []
-    colheaders = []
-    dataSchema = {}
-    for field in qa_info._meta.fields:
-        dict_name_verbose_name[field.name] = field.verbose_name
-
-        if not field.verbose_name in exclude_list:
-            print field.verbose_name
-            colheaders.append(field.verbose_name.encode("utf8"))
-            dataSchema[field.verbose_name] = ''
-            columns_item = {
-                u"title": field.verbose_name,
-                u"field": field.verbose_name,
-                # u"sortable": u"true",
-            }
-            if field.verbose_name == u"问题描述":
-                columns_item[u"width"] = u"20%"
-                columns_item[u"title"] = u"问题描述"
-            elif field.verbose_name == u"整改措施":
-                columns_item[u"width"] = u"20%"
-                columns_item[u"title"] = u"整改措施"
-            elif field.verbose_name == u"处理意见":
-                columns_item[u"width"] = u"6%"
-                columns_item[u"title"] = u"处理意见"
-            else:
-                split_list = list(field.verbose_name)
-                # every two word add
-                title_str = ""
-                for i in range(len(split_list)):
-                    title_str = title_str + split_list[i]
-                    if (i + 1) % 2 == 0:
-                        title_str = title_str + u"<br>"
-                if field.verbose_name == u"相关附件":
-                    columns_item[u'formatter'] = "attachment"
-                columns_item[u"title"] = title_str
-                columns_item[u"width"] = u"2%"
-            columns_set.append(columns_item)
-
-    json_columns = json.dumps(columns_set)
-
-    upload_data = []
-    for item in list_data:
-        upload_data.append(item['fields'])
-        # print upload_data
-
-    chinese_updata = []
-    for item in upload_data:
-        dict_updata = {}
-        for key, value in item.items():
-            if not dict_name_verbose_name[key] in exclude_list:
-                dict_updata[dict_name_verbose_name[key]] = value
-
-                # print chinese_updata
-        chinese_updata.append(dict_updata)
-
-    df_data = pd.DataFrame(chinese_updata)
+    df_data = pd.DataFrame(df_chinese_data())
     source = df_data[u"信息来源"].value_counts().to_json()
     title = u'汇总'
     return render(request, 'source.html',{'title': title, 'source': source})
 
 def source_month(request):
+    df_data = pd.DataFrame(df_chinese_data())
+    df_da = pd.DataFrame(df_chinese_data(), index=df_data[u'日期'])
     year_month = request.GET.get('value_conf', None)
-    exclude_list = [u"检查者", u"责任人", u"ID"]
 
-    query_data = qa_info.objects.all().order_by('-data')
-    json_data = serializers.serialize("json", query_data, use_natural_foreign_keys=True)
-    list_data = json.loads(json_data)
-
-    dict_name_verbose_name = {}
-    columns_set = []
-    colheaders = []
-    dataSchema = {}
-    for field in qa_info._meta.fields:
-        dict_name_verbose_name[field.name] = field.verbose_name
-
-        if not field.verbose_name in exclude_list:
-            print field.verbose_name
-            colheaders.append(field.verbose_name.encode("utf8"))
-            dataSchema[field.verbose_name] = ''
-            columns_item = {
-                u"title": field.verbose_name,
-                u"field": field.verbose_name,
-                # u"sortable": u"true",
-            }
-            if field.verbose_name == u"问题描述":
-                columns_item[u"width"] = u"20%"
-                columns_item[u"title"] = u"问题描述"
-            elif field.verbose_name == u"整改措施":
-                columns_item[u"width"] = u"20%"
-                columns_item[u"title"] = u"整改措施"
-            elif field.verbose_name == u"处理意见":
-                columns_item[u"width"] = u"6%"
-                columns_item[u"title"] = u"处理意见"
-            else:
-                split_list = list(field.verbose_name)
-                # every two word add
-                title_str = ""
-                for i in range(len(split_list)):
-                    title_str = title_str + split_list[i]
-                    if (i + 1) % 2 == 0:
-                        title_str = title_str + u"<br>"
-                if field.verbose_name == u"相关附件":
-                    columns_item[u'formatter'] = "attachment"
-                columns_item[u"title"] = title_str
-                columns_item[u"width"] = u"2%"
-            columns_set.append(columns_item)
-
-    json_columns = json.dumps(columns_set)
-
-    upload_data = []
-    for item in list_data:
-        upload_data.append(item['fields'])
-        # print upload_data
-
-    chinese_updata = []
-    for item in upload_data:
-        dict_updata = {}
-        for key, value in item.items():
-            if not dict_name_verbose_name[key] in exclude_list:
-                dict_updata[dict_name_verbose_name[key]] = value
-
-                # print chinese_updata
-        chinese_updata.append(dict_updata)
-
-    df_data = pd.DataFrame(chinese_updata)
-    df_da = pd.DataFrame(chinese_updata, index=df_data[u'日期'])
     end = arrow.get(year_month)
     end = end.replace(day=25)
     start = end.replace(months=-1)
@@ -291,70 +173,8 @@ def source_month(request):
     return HttpResponse(source)
 
 def month_count(request):
-    exclude_list = [u"检查者", u"责任人", u"ID"]
-
-    query_data = qa_info.objects.all().order_by('-data')
-    json_data = serializers.serialize("json", query_data, use_natural_foreign_keys=True)
-    list_data = json.loads(json_data)
-
-    dict_name_verbose_name = {}
-    columns_set = []
-    colheaders = []
-    dataSchema = {}
-    for field in qa_info._meta.fields:
-        dict_name_verbose_name[field.name] = field.verbose_name
-
-        if not field.verbose_name in exclude_list:
-            print field.verbose_name
-            colheaders.append(field.verbose_name.encode("utf8"))
-            dataSchema[field.verbose_name] = ''
-            columns_item = {
-                u"title": field.verbose_name,
-                u"field": field.verbose_name,
-                # u"sortable": u"true",
-            }
-            if field.verbose_name == u"问题描述":
-                columns_item[u"width"] = u"20%"
-                columns_item[u"title"] = u"问题描述"
-            elif field.verbose_name == u"整改措施":
-                columns_item[u"width"] = u"20%"
-                columns_item[u"title"] = u"整改措施"
-            elif field.verbose_name == u"处理意见":
-                columns_item[u"width"] = u"6%"
-                columns_item[u"title"] = u"处理意见"
-            else:
-                split_list = list(field.verbose_name)
-                # every two word add
-                title_str = ""
-                for i in range(len(split_list)):
-                    title_str = title_str + split_list[i]
-                    if (i + 1) % 2 == 0:
-                        title_str = title_str + u"<br>"
-                if field.verbose_name == u"相关附件":
-                    columns_item[u'formatter'] = "attachment"
-                columns_item[u"title"] = title_str
-                columns_item[u"width"] = u"2%"
-            columns_set.append(columns_item)
-
-    json_columns = json.dumps(columns_set)
-
-    upload_data = []
-    for item in list_data:
-        upload_data.append(item['fields'])
-        # print upload_data
-
-    chinese_updata = []
-    for item in upload_data:
-        dict_updata = {}
-        for key, value in item.items():
-            if not dict_name_verbose_name[key] in exclude_list:
-                dict_updata[dict_name_verbose_name[key]] = value
-
-                # print chinese_updata
-        chinese_updata.append(dict_updata)
-
-    df_data = pd.DataFrame(chinese_updata)
-    df_da = pd.DataFrame(chinese_updata, index=df_data[u'日期'])
+    df_data = pd.DataFrame(df_chinese_data())
+    df_da = pd.DataFrame(df_chinese_data(), index=df_data[u'日期'])
     string_index = df_data[u'日期']
     # 计算出起止月份
     start_day = string_index.min()
@@ -396,3 +216,6 @@ def month_count(request):
     json_count = json.dumps(list_month_count)
     return render(request, "month_count.html",{"json_month":json_month,
                                                "json_count":json_count})
+
+def classification(request):
+    return render(request, "classification.html")
